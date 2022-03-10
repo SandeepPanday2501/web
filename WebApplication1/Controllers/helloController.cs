016e5846ad7c3f403d213f1b5c7e26ca4d35fa96
KIST Student3 <student3@sddb.student>
2022-03-10 08:46:44 +0545

    Initial Commit

---
+++ WebApplication1/Controllers/helloController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class helloController : Controller
+    {
+
+        webEntities1 db;
+
+        public helloController()
+        {
+            db = new webEntities1();
+        }
+
+
+        // GET: hello
+        public ActionResult Index()
+        {
+            List<employee> all_data = db.employees.ToList();
+            return View(all_data);
+        }
+    }
+}
